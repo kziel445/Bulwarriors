9f8290f70d4821a1bd619b7100ae784dba7e53ba
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-07 23:18:18 +0200

    simple camera movement

--- Assets/Camera/Scripts/CameraMovement.cs
+++ Assets/Camera/Scripts/CameraMovement.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.MonoBehaviours;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float cameraSpeed = 10f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.y += cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position.y -= cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            position.x += cameraSpeed * Time.deltaTime;
+        }
+        if(Input.GetAxis("Mouse ScrollWheel")>0 && GetComponent<Camera>().orthographicSize>0.4)
+        {
+            GetComponent<Camera>().orthographicSize-=0.2f;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && GetComponent<Camera>().orthographicSize < 10)
+        {
+            GetComponent<Camera>().orthographicSize += 0.2f;
+        }
+        transform.position = position;
     }
 }
