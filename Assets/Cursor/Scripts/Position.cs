8795cf76e86c5a90fd6f270474de9c02846caab2
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-10-08 14:42:58 +0200

    Cursor position (#26)
    * cursor position rewrited

---
+++ Assets/Cursor/Scripts/Position.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cursor
+{
+    public class Position
+    {
+        public Vector2 getMousePosition()
+        {
+            Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+
+    }
+}
+
