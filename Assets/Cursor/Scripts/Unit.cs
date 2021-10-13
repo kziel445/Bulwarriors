c90a7df25d3b236169a7e4e2b63769ff5fd43fc7
Kamil Zieliński <kzielinski445@gmail.com>
2021-10-13 11:41:04 +0200

    add scripts for clickable objects

---
+++ Assets/Cursor/Scripts/Unit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager: MonoBehaviour, IClickable
+{
+    void Update()
+    {
+     	#TODO this whole script should be added to unit main unit script
+    }
+
+	public void Click()
+	{
+		Debug.Log("Unit");
+	}
+
+}
