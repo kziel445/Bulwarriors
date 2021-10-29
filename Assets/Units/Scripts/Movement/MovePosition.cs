e4a42df4143242c22923be2588d01c330356a431
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-29 08:02:56 +0200

    fix movement

--- Assets/Units/Scripts/Movement/MovePosition.cs
+++ Assets/Units/Scripts/Movement/MovePosition.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class MovePosition : MonoBehaviour, IMovePosition
 {
-    private Vector3 movePosition;
+    private Vector2 movePosition;
     // public Animator animator;
-    private Vector3 movement;
+    private Vector2 movement;
     
     public void Awake()
     {
         movePosition = transform.position;
     }
     // TODO: if this really necessary
-    public void SetMovePosition(Vector3 movePosition)
+    public void SetMovePosition(Vector2 movePosition)
     {
         this.movePosition = movePosition;
     }
 
     private void Update()
     {
-        Vector3 moveDir = (movePosition - transform.position).normalized;
+        Vector2 moveDir = (movePosition - new Vector2(transform.position.x, transform.position.y)).normalized;
         movement.x = moveDir.x;
         movement.y = moveDir.y;
         // animator.SetFloat("Horizontal", movement.x);
         // animator.SetFloat("Vertical", movement.y);
         // animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        if (Vector3.Distance(movePosition, transform.position) < 0.4f)
+        if (Vector2.Distance(movePosition, transform.position) < 0.4f)
         {
-            moveDir = Vector3.zero;
+            moveDir = Vector2.zero;
             movement.x = moveDir.x;
             movement.y = moveDir.y;
             // animator.SetFloat("Horizontal", movement.x);
