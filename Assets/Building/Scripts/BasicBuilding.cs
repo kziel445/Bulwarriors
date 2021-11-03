82934f5d64ef19c7e5646448b0f59599518d4bdd
Kamil Zieliński <kzielinski445@gmail.com>
2021-11-03 13:34:56 +0100

    building script

---
+++ Assets/Building/Scripts/BasicBuilding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Building
+{
+    public class BasicBuilding : ScriptableObject
+    {
+
+    }
+
+}
--- Assets/Building/Scripts/BuildingRTS.cs
+++ Assets/Building/Scripts/BuildingRTS.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingRTS : MonoBehaviour, IClickable
+namespace Building
 {
-    public void Click()
+    public class BuildingRTS : MonoBehaviour, IClickable
     {
-        Debug.Log("Building options in UI");
-        Debug.Log("Building health");
+        public void Click()
+        {
+            Debug.Log("Building options in UI");
+            Debug.Log("Building health");
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
