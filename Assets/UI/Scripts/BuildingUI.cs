8ca2d9e00e1412122276d18076760fec2458bdf5
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-11-18 07:56:40 +0100

    health of Building in UI

--- Assets/UI/Scripts/BuildingUI.cs
+++ Assets/UI/Scripts/BuildingUI.cs
@@ -12,10 +12,20 @@ namespace UI
         {
             if(ActionFrame.instance!=null)
             {
-                if (visible) ActionFrame.instance.SetActionButtonsBuilding(gameObject.GetComponent<Buildings.Player.PlayerBuilding>().baseStats.actions, gameObject.transform);
+                if (visible) ActionFrame.instance.SetActionButtonsBuilding(
+                    gameObject.GetComponent<Buildings.Player.PlayerBuilding>().baseStats.actions, 
+                    gameObject.transform
+                    );
                 else if (!visible) ActionFrame.instance.ClearActions();
             }
-
+            if(StatisticsFrame.instance!=null)
+            {
+                if (visible) StatisticsFrame.instance.ChangeStatsOfObject(
+                    gameObject.transform.Find("StatsDisplay").GetComponent<Core.HealthHandler>().currentHealth,
+                    gameObject.transform.Find("StatsDisplay").GetComponent<Core.HealthHandler>().baseHealth
+                    );
+                else if (!visible) Debug.Log("disabledStats");//ActionFrame.instance.ClearActions();
+            }
             base.SetSelectedVisible(visible);
         }
 
--- Assets/UI/Scripts/Statistics/StatisticsFrame.cs
+++ Assets/UI/Scripts/Statistics/StatisticsFrame.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class StatisticsFrame : MonoBehaviour
     {
-        public void ShowStatsOfObject()
+        public static StatisticsFrame instance;
+        private void Awake()
+        {
+            instance = this;
+        }
+        public void ChangeStatsOfObject(float currentHealth, float maxHealth)
+        {
+            gameObject.GetComponentInChildren<Text>().text= $"Health: {currentHealth}	Max health: {maxHealth}";
+        }
+        public void Clear()
         {
 
+            //better would be visible off
+            Debug.Log("Clear Stats");
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
