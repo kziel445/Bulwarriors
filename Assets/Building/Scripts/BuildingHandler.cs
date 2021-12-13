a59249b88b26f963cbf8243c4e360fd1fec4792d
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-12-13 21:10:46 +0100

    add buildings basics

--- Assets/Building/Scripts/BuildingHandler.cs
+++ Assets/Building/Scripts/BuildingHandler.cs
@@ -7,8 +7,8 @@ namespace Buildings
     public class BuildingHandler : MonoBehaviour
     {
         public static BuildingHandler instance;
-        [SerializeField]
-        public BuildingBasic house, barrack;
+        [SerializeField] public BuildingBasic 
+            archeryRange, bank, barrack, citadel, farm, house, shootingRange, stable;
         private void Awake()
         {
             instance = this;
@@ -18,11 +18,29 @@ namespace Buildings
             BuildingBasic building;
             switch (type)
             {
+                case "archeryRange":
+                    building = archeryRange;
+                    break;
+                case "bank":
+                    building = bank;
+                    break;
                 case "barrack":
                     building = barrack;
                     break;
+                case "citadel":
+                    building = citadel;
+                    break;
+                case "farm":
+                    building = farm;
+                    break;
                 case "house":
                     building = house;
+                    break;
+                case "shootingRange":
+                    building = shootingRange;
+                    break;
+                case "stable":
+                    building = stable;
                     break;
                 default:
                     Debug.Log($"Building Type: {type} could not be found or does not exist!");
